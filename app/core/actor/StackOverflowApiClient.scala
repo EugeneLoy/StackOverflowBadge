b71b8f1339bf7f8bdfb2cd89e2b223534055fe8c
Eugeny Loy <eugeny.loy@gmail.com>
2014-09-24 10:42:46 +0300

    Added priorities to SO client messages

--- app/core/actor/StackOverflowApiClient.scala
+++ app/core/actor/StackOverflowApiClient.scala
@@ -1,7 +1,9 @@
 package core.actor
 
-import akka.actor.{ActorRef, Props, Actor, FSM}
+import akka.actor._
 import akka.contrib.throttle.TimerBasedThrottler
+import akka.dispatch.{UnboundedPriorityMailbox, PriorityGenerator}
+import com.typesafe.config.Config
 import core.stackoverflow.StackOverflowApi
 import play.api.Logger
 import scala.concurrent.duration._
@@ -27,17 +29,28 @@ object StackOverflowApiClient {
   case class Perform(request: Request, replyTo: ActorRef)
   case class Deliver(response: Response, to: ActorRef, originalRequest: Request)
 
-  def props = Props(new StackOverflowApiClient(new StackOverflowApi))
+  case class BackOff()
+
+  def props = Props(classOf[StackOverflowApiClient], new StackOverflowApi, 1 msgsPer (100 milliseconds)).withDispatcher("stack-overflow-api-client-dispatcher")
+
+  def priorityGenerator = PriorityGenerator {
+    case _: BackOff => 0
+    case _: Perform => 1
+    case _: Request => 2
+    case _ => 3
+  }
 
 }
 
 import StackOverflowApiClient._
 
-class StackOverflowApiClient(api: StackOverflowApi) extends Actor with FSM[State, Unit] {
+class StackOverflowApiClientMailbox(settings: ActorSystem.Settings, config: Config) extends UnboundedPriorityMailbox(priorityGenerator)
+
+class StackOverflowApiClient(api: StackOverflowApi, requestRate: Rate) extends Actor with FSM[State, Unit] {
 
   val logger = Logger(this.getClass())
 
-  val throttler = context.actorOf(Props(new TimerBasedThrottler(1 msgsPer (1 second)))) // TODO
+  val throttler = context.actorOf(Props(classOf[TimerBasedThrottler], requestRate))
   throttler ! SetTarget(Some(self))
 
   def wrapResponse(originalRequest: Request, replyTo: ActorRef)(rawApiResponse: (Int, JsValue)) = {
@@ -53,8 +66,8 @@ class StackOverflowApiClient(api: StackOverflowApi) extends Actor with FSM[State
   startWith(Active, ())
 
   when(Active) {
-    case Event(message: Request, _) =>
-      throttler ! Perform(message, sender)
+    case Event(request: Request, _) =>
+      throttler ! Perform(request, sender)
       stay
     case Event(Perform(request, replyTo), _) =>
       perform(request, replyTo)
@@ -63,6 +76,8 @@ class StackOverflowApiClient(api: StackOverflowApi) extends Actor with FSM[State
       to ! response
       // TODO handle backoff here
       stay
+    case Event(BackOff(), _) =>
+      goto(BackingOff)
   }
 
   when(BackingOff) {
