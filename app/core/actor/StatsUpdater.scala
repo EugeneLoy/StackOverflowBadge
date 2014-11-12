11789ccd7584b775ba6abc80eb5b9f5e1848b9a0
Eugeny Loy <eugeny.loy@gmail.com>
2014-11-12 21:09:08 +0200

    cleanup

--- app/core/actor/StatsUpdater.scala
+++ app/core/actor/StatsUpdater.scala
@@ -137,7 +137,7 @@ class StatsUpdater(
   def updatingStats: Receive = {
     case TagPersister.TagsPersisted =>
       log.info("tags persisted - erasing journal and restarting process")
-      deleteMessages(lastSequenceNr)
+      deleteMessages(lastSequenceNr, true)
       fetchTagList
     case Recover =>
       startUpdatingStats
@@ -158,62 +158,3 @@ class StatsUpdater(
   // TODO handle unhandled
 
 }
-
-
-
-//class StatsUpdater(apiClient: ActorRef) extends Actor with ActorLogging with RandomIdGenerator with Subtasks {
-//
-//  import StatsUpdater._
-//
-//  context.watch(apiClient)
-//
-//  def collection: JSONCollection = db.collection[JSONCollection]("rate_tops")
-//
-//  var tags = Set.empty[Tag]
-//
-//  start(TagListFetcher.props(apiClient), TagListFetcher.actorName)
-//
-//  def calculateTops(rates: Seq[Double]) = {
-//    val data = rates.sorted.reverse
-//    (for (p <- 10 to 50 by 10) yield (p, data(data.size * p / 100))).toMap
-//  }
-//
-//  def fetchingTagList: Receive = {
-//    case TagsFetched(tags) if subtasks contains sender =>
-//      complete(sender)
-//      for (tagName <- tags) {
-//         start(TagStatsUpdater.props(apiClient, tagName), TagStatsUpdater.actorName(tagName))
-//      }
-//      context.become(updatingTagStats)
-//  }
-//
-//  def updatingTagStats: Receive = {
-//    case TagUpdated(tag) if subtasks contains sender =>
-//      complete(sender)
-//      tags += tag
-//      if (subtasks.isEmpty) {
-//        val tops = calculateTops(tags.map(_.rate).toSeq)
-//        val rateTops = RateTops("singleton", tops(10), tops(20), tops(30), tops(40), tops(50), DateTime.now)
-//        val persistingId = nextId
-//        (for (lastError <- collection.save(rateTops) if !lastError.inError) yield RateTopsPersisted(persistingId)).pipeTo(self)
-//        context.become(persisting(persistingId))
-//      }
-//  }
-//
-//  def persisting(persistingId: String): Receive = {
-//    case RateTopsPersisted(`persistingId`) =>
-//      context.parent ! StatsUpdated()
-//      context.stop(self)
-//  }
-//
-//  override def unhandled(message: Any) = {
-//    (
-//      discardUnhandled(log)(classOf[TagsFetched], classOf[TagUpdated], classOf[RateTopsPersisted])
-//        orElse throwOnNonTerminated
-//        orElse PartialFunction(super.unhandled _)
-//    )(message)
-//  }
-//
-//  override def receive: Receive = fetchingTagList
-//
-//}
